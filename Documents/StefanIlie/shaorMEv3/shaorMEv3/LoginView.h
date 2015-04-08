6fe14074fda4165c81b12783434b6338be0bdf60
Razvan Rusu <rusurazvangabriel@gmail.com>
2015-04-08 17:45:19 +0300

    add recent work

---
+++ Documents/StefanIlie/shaorMEv3/shaorMEv3/LoginView.h
@@ -0,0 +1,15 @@
+//
+//  LoginView.h
+//  shaorMEv3
+//
+//  Created by LaboratoriOS Cronian Academy on 01/04/15.
+//  Copyright (c) 2015 LaboratoriOS Cronian Academy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface LoginView : UIViewController <PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate>
+
+
+@end
---
+++ Documents/StefanIlie/shaorMEv3/shaorMEv3/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  shaorMEv3
+//
+//  Created by LaboratoriOS Cronian Academy on 01/04/15.
+//  Copyright (c) 2015 LaboratoriOS Cronian Academy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor blueColor];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
